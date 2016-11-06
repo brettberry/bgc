d3eb1a7ac26fe314b513115a0524df932f0383f3
Brett Berry <brett.elizabeth.berry@gmail.com>
2016-11-05 20:26:29 -0700

    initial commit

--- src/Home.js
+++ src/Home.js
@@ -1,34 +1,9 @@
 import React, { Component } from 'react';
-import Header from './Header';
-import Subheader from './Subheader';
 
-const styles = {
-  container: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingTop: '80px',
-    flexDirection: 'column'
-  },
-  contentDiv: {
-    display: 'flex',
-    height: '350px',
-    width: '950px',
-    backgroundColor: 'rgba(0,0,0,0)',
-    justifyContent: 'center'
-  }
-};
 
 class Home extends Component {
 
-  render() {
-    return (
-      <div style={styles.container}>
-        <Header />
-        <Subheader />
-      </div>
-    );
-  }
+  render() {}
 }
 
 export default Home;
