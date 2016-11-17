6493fca3e78212b00cad50cc8686005bdd49b652
Brett Berry <brett.elizabeth.berry@gmail.com>
2016-11-16 21:48:43 -0800

    Products page sidebar.

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,12 +1,57 @@
 import React, { Component } from 'react';
+import FaChevronCircleUp from 'react-icons/lib/fa/chevron-circle-up';
 import './products.styles.scss';
 
 export default class Product extends Component {
   render() {
     return (
       <div>
-        <h1>products</h1>
+        <Sidebar />
+        {/* <ProductGrid /> */}
       </div>
     );
   }
+}
+
+function Sidebar() {
+  return (
+    <div className="sidebarContainer">
+      <div className="categoryContainer">
+        <FaChevronCircleUp className="downArrow"/>
+        <h3 className="callType">Most Popular</h3>
+      </div>
+      <div className="categoryContainer">
+        <FaChevronCircleUp className="downArrow"/>
+        <h3 className="callType">Online Specials</h3>
+      </div>
+      <div className="categoryContainer">
+        <FaChevronCircleUp className="downArrow"/>
+        <h3 className="callType">Thunder Bugles</h3>
+      </div>
+      <div className="categoryContainer">
+        <FaChevronCircleUp className="downArrow"/>
+        <h3 className="callType">Golden Dome Reeds</h3>
+      </div>
+      <div className="categoryContainer">
+        <FaChevronCircleUp className="downArrow"/>
+        <h3 className="callType">X-Series</h3>
+      </div>
+      <div className="categoryContainer">
+        <FaChevronCircleUp className="downArrow"/>
+        <h3 className="callType">Original Big Bull Reeds</h3>
+      </div>
+      <div className="categoryContainer">
+        <FaChevronCircleUp className="downArrow"/>
+        <h3 className="callType">Cow Calls</h3>
+      </div>
+      <div className="categoryContainer">
+        <FaChevronCircleUp className="downArrow"/>
+        <h3 className="callType">Hunting Movies</h3>
+      </div>
+      <div className="categoryContainer">
+        <FaChevronCircleUp className="downArrow"/>
+        <h3 className="callType">Other</h3>
+      </div>
+    </div>
+  );
 }
