17c094dec2bccaeba0f6ae1d0519ab06b4b85f63
Brett Berry <brett.elizabeth.berry@gmail.com>
2016-11-20 16:10:40 -0800

    Footer/Contact form layout.

--- src/Home.js
+++ src/Home.js
@@ -37,7 +37,27 @@ class Home extends Component {
               </div>
               <p className="addressText">Medical Lake, WA 99022</p>
             </div>
-            <div className="rightContents"></div>
+            <div className="rightContents">
+              <h3 className="contactSubheader">Send us a message</h3>
+              <div className="callDiv">
+                <FaUser className="phoneIcon" />
+                <input className="singleLineInput"
+                       placeholder="Name"/>
+              </div>
+              <div className="callDiv">
+                <FaEnvelopeO className="phoneIcon" />
+                <input className="singleLineInput"
+                       placeholder="Email"/>
+              </div>
+              <div className="callDiv">
+                <FaComment className="phoneIcon" />
+                <textarea className="textArea"
+                          placeholder="Message"/>
+              </div>
+              <div className="sendDiv">
+                <Button text="send"/>
+              </div>
+            </div>
           </div>
         </div>
       </div>
