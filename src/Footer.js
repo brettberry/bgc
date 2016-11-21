9ec1096f89e9cba3749d91ab0db029124fb05509
Brett Berry <brett.elizabeth.berry@gmail.com>
2016-11-20 16:26:06 -0800

    Fixed bugs.

--- src/Footer.js
+++ src/Footer.js
@@ -1,8 +1,71 @@
-// import React, { Component } from 'react';
-// import './footer.styles.scss';
-//
-// class Footer extends Component {
-//
-// }
-//
-// export default Footer;
+import React, { Component } from 'react';
+import FaPhone from 'react-icons/lib/fa/phone';
+import FaEnvelope from 'react-icons/lib/fa/envelope';
+import FaEnvelopeO from 'react-icons/lib/fa/envelope-o';
+import FaComment from 'react-icons/lib/fa/comment-o';
+import FaUser from 'react-icons/lib/fa/user';
+import Button from './Buttons';
+import './buttons.styles.scss';
+import './footer.styles.scss';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="footer">
+        <h3 className="contactHeader">Contact us</h3>
+        <div className="horizontalRule"/>
+        <div className="contactInfoContainer">
+          <LeftSide />
+          <RightSide />
+        </div>
+      </div>
+    );
+  }
+}
+
+function LeftSide() {
+  return (
+    <div className="leftContents">
+      <h3 className="contactSubheader">Berry Game Calls</h3>
+      <div className="callDiv">
+        <FaPhone className="phoneIcon"/>
+        <p className="contactText">Monday-Friday 9am-5pm PST</p>
+      </div>
+      <p className="addressText">1 (800) 434-2855 </p>
+      <p className="addressText">1 (509) 299-5524 </p>
+      <div className="callDiv">
+        <FaEnvelope className="phoneIcon" />
+        <p className="contactText">PO Box 416 / 219 S. Lefevre</p>
+      </div>
+      <p className="addressText">Medical Lake, WA 99022</p>
+    </div>
+  );
+}
+
+function RightSide() {
+  return (
+    <div className="rightContents">
+      <h3 className="contactSubheader">Send us a message</h3>
+      <div className="callDiv">
+        <FaUser className="phoneIcon" />
+        <input className="singleLineInput"
+               placeholder="Name"/>
+      </div>
+      <div className="callDiv">
+        <FaEnvelopeO className="phoneIcon" />
+        <input className="singleLineInput"
+               placeholder="Email"/>
+      </div>
+      <div className="callDiv">
+        <FaComment className="phoneIcon" />
+        <textarea className="textArea"
+                  placeholder="Message"/>
+      </div>
+      <div className="sendDiv">
+        <Button text="send"/>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
--- src/Home.js
+++ src/Home.js
@@ -3,11 +3,7 @@ import FaChevronLeft from 'react-icons/lib/fa/chevron-left';
 import FaChevronRight from 'react-icons/lib/fa/chevron-right';
 import { Link } from 'react-router';
 import Button from './Buttons';
-import FaPhone from 'react-icons/lib/fa/phone';
-import FaEnvelope from 'react-icons/lib/fa/envelope';
-import FaEnvelopeO from 'react-icons/lib/fa/envelope-o';
-import FaComment from 'react-icons/lib/fa/comment-o';
-import FaUser from 'react-icons/lib/fa/user';
+import Footer from './Footer';
 import './buttons.styles.scss';
 import './home.styles.scss';
 
@@ -19,47 +15,7 @@ class Home extends Component {
         <Featured />
         <DemoSection />
         <ShopCategories />
-        <div className="footer">
-          <h3 className="contactHeader">Contact us</h3>
-          <div className="horizontalRule"/>
-          <div className="contactInfoContainer">
-            <div className="leftContents">
-              <h3 className="contactSubheader">Berry Game Calls</h3>
-              <div className="callDiv">
-                <FaPhone className="phoneIcon"/>
-                <p className="contactText">Monday-Friday 9am-5pm PST</p>
-              </div>
-              <p className="addressText">1 (800) 434-2855 </p>
-              <p className="addressText">1 (509) 299-5524 </p>
-              <div className="callDiv">
-                <FaEnvelope className="phoneIcon" />
-                <p className="contactText">PO Box 416 / 219 S. Lefevre</p>
-              </div>
-              <p className="addressText">Medical Lake, WA 99022</p>
-            </div>
-            <div className="rightContents">
-              <h3 className="contactSubheader">Send us a message</h3>
-              <div className="callDiv">
-                <FaUser className="phoneIcon" />
-                <input className="singleLineInput"
-                       placeholder="Name"/>
-              </div>
-              <div className="callDiv">
-                <FaEnvelopeO className="phoneIcon" />
-                <input className="singleLineInput"
-                       placeholder="Email"/>
-              </div>
-              <div className="callDiv">
-                <FaComment className="phoneIcon" />
-                <textarea className="textArea"
-                          placeholder="Message"/>
-              </div>
-              <div className="sendDiv">
-                <Button text="send"/>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Footer />
       </div>
     );
   }
