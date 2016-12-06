0c3527655e03977893d7c47c6513d147aae5cf00
Brett Berry <brett.elizabeth.berry@gmail.com>
2016-12-05 17:32:23 -0800

    Working nav bars for desktop and tablet.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import NavigationBar from './NavigationBar';
 import TabletNavigationBar from './TabletNavigationBar';
+import TabletProvider from './TabletProvider';
 import './app.styles.scss';
 
 export default class App extends Component {
   render() {
+    return (
+      <TabletProvider>
+        <NavBarRenderer />
+      </TabletProvider>
+    );
+  }
+}
+
+class NavBarRenderer extends Component {
+
+  static propTypes = {
+    isTablet: PropTypes.bool
+  }
+
+  static defaultProps = {
+    isTablet: false
+  }
+
+  render() {
+    return this.props.isTablet ? this.renderTablet() : this.renderDesktop();
+  }
+
+  renderTablet() {
     const { children } = this.props;
     return (
       <div>
         <TabletNavigationBar className="navBarHeader" />
+        {children}
+      </div>
+    );
+  }
+
+  renderDesktop() {
+    const { children } = this.props;
+    return (
+      <div>
+        <NavigationBar className="navBarHeader" />
         {children}
       </div>
     );
