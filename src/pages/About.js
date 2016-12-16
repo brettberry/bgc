fb6e68dd33e71780283baa5c2e27b176b0461210
Brett Berry <brett.elizabeth.berry@gmail.com>
2016-12-16 13:47:41 -0800

    Random fixes for Safari.

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,12 +4,10 @@ import './about.styles.scss';
 class About extends Component {
   render() {
     return (
-      <div className="aboutPageContainer">
-        <div className="aboutContainer">
-          <p className="aboutText">Glen Berry name is synonymous with elk calling. As of 2016, he shot his 55 bull elk with Archery equipment. Glen produced his first VHS tape in 1985 called ‘Bugling with success.’ Montana guide schools used this tape and any new elk video’s he released to help teach their students Glen’s elk calling techniques. Glen has produced programming for the Outdoor Channel, Sportsman Channel, Men’s channel and has been a big part in Berry Game Calls VHS and DVD Bowhunting Series. </p>
-          <p className="aboutText">Glen is a seminar speaker at the largest Sportsmen shows in the Northwest for the past 30 years. Glen’s creative call designs lead to innovation for Berry Game Calls, hunters have been using these calls for the past 30 years with great success.</p>
-          <p className="aboutText">Glen and Liz Berry established Berry Game Calls in 1983.</p>
-        </div>
+      <div className="aboutContainer">
+        <p className="aboutText">Glen Berry name is synonymous with elk calling. As of 2016, he shot his 55 bull elk with Archery equipment. Glen produced his first VHS tape in 1985 called ‘Bugling with success.’ Montana guide schools used this tape and any new elk video’s he released to help teach their students Glen’s elk calling techniques. Glen has produced programming for the Outdoor Channel, Sportsman Channel, Men’s channel and has been a big part in Berry Game Calls VHS and DVD Bowhunting Series. </p>
+        <p className="aboutText">Glen is a seminar speaker at the largest Sportsmen shows in the Northwest for the past 30 years. Glen’s creative call designs lead to innovation for Berry Game Calls, hunters have been using these calls for the past 30 years with great success.</p>
+        <p className="aboutText">Glen and Liz Berry established Berry Game Calls in 1983.</p>
       </div>
     );
   }
