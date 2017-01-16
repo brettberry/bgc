06e4cb117eb817ac5b0836ec251fd01e0e636b73
Brett Berry <brett.elizabeth.berry@gmail.com>
2017-01-16 11:50:31 -0800

    Fixed footer bug.

--- src/Footer.js
+++ src/Footer.js
@@ -15,8 +15,6 @@ class Footer extends Component {
   render() {
     return (
       <div className="footer">
-        {/* <h3 className="header">Contact us</h3> */}
-        {/* <div className="horizontalRule"/> */}
         <div className="contactContainer">
           <LeftSide />
           <RightSide />
@@ -32,37 +30,37 @@ function LeftSide() {
     <div className="leftContents">
       <h3 className="subheader">Berry Game Calls</h3>
       <div className="section">
-        <div className="itemContainer">
+        <div className="infoContainer">
           <FaPhone className="icon" />
-          <p className="item">Monday-Friday 9am-5pm PST</p>
+          <p className="iconText">Monday-Friday 9am-5pm PST</p>
         </div>
         <p className="text">1 (800) 434-2855 </p>
         <p className="text">1 (509) 299-5524 </p>
       </div>
       <div className="section">
-        <div className="itemContainer">
+        <div className="infoContainer">
           <FaEnvelope className="icon" />
-          <p className="item">PO Box 416</p>
+          <p className="iconText">PO Box 416</p>
         </div>
         <p className="text">Medical Lake, WA 99022</p>
       </div>
       <div className="section">
-        <div className="itemContainer">
+        <div className="infoContainer">
           <FaFacebook className="icon" />
           <a target="_blank"
              className="anchor"
              href="https://www.facebook.com/BerryGameCalls/?fref=ts">
-            <p className="item">Follow us on Facebook</p>
+            <p className="iconText">Follow us on Facebook</p>
           </a>
         </div>
       </div>
       <div className="section">
-        <div className="itemContainer">
+        <div className="infoContainer">
           <FaYoutube className="icon" />
           <a target="_blank"
              className="anchor"
              href="https://www.youtube.com/user/BERRYGAMECALLS">
-            <p className="item">Subscribe to our Youtube Channel</p>
+            <p className="iconText">Subscribe to our Youtube Channel</p>
           </a>
         </div>
       </div>
