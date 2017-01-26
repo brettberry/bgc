a57e5e3bc2a2fd4d9c7c390b078fe16f29396a7f
Brett Berry <brett.elizabeth.berry@gmail.com>
2017-01-25 16:06:39 -0800

    Added product photo files.

---
+++ src/models/ShippingInfoModel.js
@@ -0,0 +1,41 @@
+import Model from './Model';
+
+export default class ShippingInfoModel extends Model {
+
+  getFirstName() {
+    return this.get('firstName');
+  }
+
+  getLastName() {
+    return this.get('lastName');
+  }
+
+  getAddressLine1() {
+    return this.get('addressLine1');
+  }
+
+  getAddressLine2() {
+    return this.get('addressLine2');
+  }
+
+  getCity() {
+    return this.get('city');
+  }
+
+  getState() {
+    return this.get('state');
+  }
+
+  getZip() {
+    return this.get('zip');
+  }
+
+  getUserId() {
+    return this.get('userId');
+  }
+
+  getId() {
+    return this.get('id');
+  }
+
+}
