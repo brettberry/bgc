6569dee6533a0f58f9ddb429435d379d87b74fdd
Brett Berry <brett.elizabeth.berry@gmail.com>
2017-03-29 13:28:36 -0700

    Fixed styling bugs.

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,21 +5,21 @@ class About extends Component {
   render() {
     return (
       <div className="aboutContainer">
-        <div className="section">
+        <div className="aboutSection">
           <div className="aboutPhotoContainer">
             <div style={{ backgroundImage: 'url(/bgcHuntingPhotos/glen-elk-29.jpg)' }}
                  className="portrait"/>
             <span className="caption">Glen Berry with the cover bull from Bugling With Success, 1985</span>
           </div>
           <div className="textContainer">
-            <h2 className="title">Beginnings</h2>
+            <h2 className="aboutTitle">Beginnings</h2>
             <div className="line"/>
             <p className="aboutText">Glen Berry name is synonymous with elk calling. As of 2016, he shot his 55 bull elk with Archery equipment. Glen produced his first VHS tape in 1985 called ‘Bugling with success.’ Montana guide schools used this tape and any new elk video’s he released to help teach their students Glen’s elk calling techniques. Glen has produced programming for the Outdoor Channel, Sportsman Channel, Men’s channel and has been a big part in Berry Game Calls VHS and DVD Bowhunting Series. </p>
           </div>
         </div>
-      <div className="section">
+      <div className="aboutSection">
         <div className="textContainer">
-          <h2 className="title">Over the years</h2>
+          <h2 className="aboutTitle">Over the years</h2>
           <div className="line"/>
           <p className="aboutText">Glen Berry name is synonymous with elk calling. As of 2016, he shot his 55th bull elk with Archery equipment. Glen produced his first VHS tape in 1985 called ‘Bugling with success.’ Montana guide schools used this tape and any new elk video’s he released to help teach their students Glen’s elk calling techniques. Glen has produced programming for the Outdoor Channel, Sportsman Channel, Men’s channel and has been a big part in Berry Game Calls VHS and DVD Bowhunting Series. </p>
         </div>
@@ -29,14 +29,14 @@ class About extends Component {
           <span className="caption">Glen Berry with son Chad's first bull elk, 1995</span>
         </div>
       </div>
-      <div className="section">
+      <div className="aboutSection">
         <div className="aboutPhotoContainer">
           <div style={{ backgroundImage: 'url(/bgcHuntingPhotos/brett-glen-deer.jpg)' }}
                className="landscape"/>
           <span className="caption">Glen Berry with daughter Brett showing off their whitetails, 2012</span>
         </div>
         <div className="textContainer">
-          <h2 className="title">Today</h2>
+          <h2 className="aboutTitle">Today</h2>
           <div className="line"/>
           <p className="aboutText">Glen Berry name is synonymous with elk calling. As of 2016, he shot his 55 bull elk with Archery equipment. Glen produced his first VHS tape in 1985 called ‘Bugling with success.’ Montana guide schools used this tape and any new elk video’s he released to help teach their students Glen’s elk calling techniques. Glen has produced programming for the Outdoor Channel, Sportsman Channel, Men’s channel and has been a big part in Berry Game Calls VHS and DVD Bowhunting Series. </p>
         </div>
