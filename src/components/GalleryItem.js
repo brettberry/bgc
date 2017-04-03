635f439b4d85298a3f3f4a51af5acc0e7b51dacc
Brett Berry <brett.elizabeth.berry@gmail.com>
2017-04-03 14:34:32 -0700

    refactored naviagtion menu items into separate classes

---
+++ src/components/GalleryItem.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+import classnames from 'classnames';
+
+import GalleryMenu from './GalleryMenu';
+import './galleryMenu.styles.scss';
+
+class GalleryItem extends Component {
+
+  state = {
+    showDropDown: false
+  }
+
+  showDropDown() {
+    this.setState({ showDropDown: true });
+    clearTimeout(this.timeout);
+  }
+
+  closeDropDown() {
+    this.timeout = setTimeout(() => {
+      this.setState({ showDropDown: false });
+    }, 300);
+  }
+
+  render() {
+    return (
+      <div className={classnames('menuItem', this.state.showDropDown && 'showGalleryMenu')}
+           onMouseEnter={() => this.showDropDown()}
+           onMouseLeave={() => this.closeDropDown()}>
+        <Link to="/gallery"
+              className="menuLink"
+              onClick={() => this.setState({ showDropDown: false })}>
+          <h3 className="item">Gallery</h3>
+          <div className="underline"/>
+        </Link>
+        <GalleryMenu className="galleryMenu"
+                     onItemClick={() => this.setState({ showDropDown: false })}/>
+      </div>
+    );
+  }
+}
+
+export default GalleryItem;
